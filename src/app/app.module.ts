44965f2f28f40fab5cf196ff41300ac6d3597b2e
Tomasz Jąder <tomasz.jader@gmail.com>
2025-04-30 23:36:44 +0200

    removed unnecessary components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { EmployeeListComponent } from './components/employee-list/employee-list.component';
-import { EmployeeListItemComponent } from './components/employee-list-item/employee-list-item.component';
 import { EmployeeSelectorComponent } from './components/employee-selector/employee-selector.component';
 import { EmployeeHierarchyComponent } from './components/employee-hierarchy/employee-hierarchy.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
@@ -11,8 +9,6 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeListComponent,
-    EmployeeListItemComponent,
     EmployeeSelectorComponent,
     EmployeeHierarchyComponent
   ],
